88d14ccff92066aba734b9467f93ecbe841c80e9
academyitwill <jhyun9682@naver.com>
2022-07-04 19:35:23 +0900

    ..

--- src/com/movie/dao/ReservationSQL.java
+++ src/com/movie/dao/ReservationSQL.java
@@ -2,13 +2,17 @@ package com.movie.dao;
 
 public class ReservationSQL {
 	public static final String Reservation_INSERT
-	="insert into reservation_info(cus_reservation_no,cus_id,payment_no)"
-	+"values(to_number(to_char(sysdate, 'yymmddHH24MI')),?,?)";
+	="insert into reservation_info(reservation_no,cus_id,payment_no)"
+	+"values(to_number(to_char(sysdate, 'yymmddHH24MISS')),?,?)";
 	
 	public static final String Reservation_SELECT_ALL
-	="select cus_reservation_no,cus_id,payment_no from reservation_info";
+	="select reservation_no,cus_id,payment_no from reservation_info";
+	
+	public static final String Reservation_SELECT_BY_RE_NO
+	="select reservation_no,cus_id,payment_no from reservation_info where reservation_no=?";
+	
 	
 	public static final String Reservation_DELETE
-	="delete from reservation_info where cus_reservation_no=?";
+	="delete from reservation_info where reservation_no=?";
 ;
 }
--- src/com/movie/dto/Reservation.java
+++ src/com/movie/dto/Reservation.java
@@ -4,14 +4,34 @@ public class Reservation {
 	private int reservation_no;
 	private String cus_id;
 	private int payment_no;
+	private Payment payment;
 	
-	public Reservation(int reservation_no, String cus_id, int payment_no) {
+	
+	
+	public Reservation(int reservation_no, String cus_id, int payment_no, Payment payment) {
 		super();
 		this.reservation_no = reservation_no;
-		this.cus_id=cus_id;
-		this.payment_no=payment_no;
-		
+		this.cus_id = cus_id;
+		this.payment_no = payment_no;
+		this.payment = payment;
 	}
+	
+	public int getReservation_no() {
+		return reservation_no;
+	}
+
+	public void setReservation_no(int reservation_no) {
+		this.reservation_no = reservation_no;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public String getCus_id() {
 		return cus_id;
 	}
@@ -24,18 +44,13 @@ public class Reservation {
 	public void setPayment_no(int payment_no) {
 		this.payment_no = payment_no;
 	}
-	public int getreservation_no() {
-		return reservation_no;
-	}
-	public void setreservation_no(int cus_reservation_no) {
-		this.reservation_no = reservation_no;
-	}
-	
-	
+
 	@Override
 	public String toString() {
-		return "Reservation [reservation_no=" + reservation_no + ", cus_id=" + cus_id + ", payment_no=" + payment_no + "]";
+		return "Reservation [reservation_no=" + reservation_no + ", cus_id=" + cus_id + ", payment_no=" + payment_no
+				+ ", payment=" + payment + "]";
 	}
+	
 	
 		
 }
