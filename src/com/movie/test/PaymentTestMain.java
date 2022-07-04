39b0d8f3965b0c50e60e999c319ef89760e941ab
academyitwill <whlee0668@naver.com>
2022-07-04 19:40:05 +0900

    ..

--- src/com/movie/test/PaymentTestMain.java
+++ src/com/movie/test/PaymentTestMain.java
@@ -1,16 +1,20 @@
 package com.movie.test;
 
 import com.movie.dao.PaymentDao;
+import com.movie.dto.Payment;
 
 public class PaymentTestMain {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		PaymentDao payment = new PaymentDao();
 		System.out.println("1.결제번호 부여");
-		//System.out.println(">>"+paymentDao.insertPayment);
+		System.out.println(">>"+payment.insertPayment(new Payment(1, "aa", 1, null, 2, 2)));
 		
 		System.out.println("2.걸제정보 출력");
-		//System.out.println(">> "+payment.selectAll());
+		System.out.println(">> "+payment.selectByNo(1));
+		
+		System.out.println("3.결제 취소");
+		System.out.println(">> "+payment.deleteByNo(1));
 	}
 }
