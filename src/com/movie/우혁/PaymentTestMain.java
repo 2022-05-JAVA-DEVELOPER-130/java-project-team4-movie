4966a5e151f189a35172a4aa60a274635e6ba27e
academyitwill <whlee0668@naver.com>
2022-07-04 14:42:24 +0900

    ..

--- src/com/movie/우혁/PaymentTestMain.java
+++ src/com/movie/우혁/PaymentTestMain.java
@@ -6,9 +6,9 @@ public class PaymentTestMain {
 		
 		PaymentDao payment = new PaymentDao();
 		System.out.println("1.결제번호 부여");
-		//System.out.println(">>"+paymentDao.insertPayment);
+		//System.out.println(">>"+payment.insertPayment("asdf"));
 		
 		System.out.println("2.걸제정보 출력");
-		//System.out.println(">> "+payment.selectAll());
+		//System.out.println(">> "+payment.selectBYNo());
 	}
 }
