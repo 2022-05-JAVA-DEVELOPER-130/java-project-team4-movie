0ba4f4791fa3e7870adead6b54c2f89733dc3719
seongmin <posteto@naver.com>
2022-07-05 20:48:00 +0900

    ..

--- src/com/movie/성민/PaymentSelect.java
+++ src/com/movie/성민/PaymentSelect.java
@@ -2,6 +2,7 @@ package com.movie.성민;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.movie.common.DataSource;
 
@@ -11,11 +12,20 @@ public class PaymentSelect {
 	public PaymentSelect() {
 		dataSource = new DataSource();
 	}
-	String sql = "SELECT m.m_name, p.cus_id, m.hall_name, s.seat_no, m.m_start_time, m.m_end_time, (pi.adult_price*p.adult_member_count) + (pi.child_price*p.child_member_count), p.card_name, sysdate FROM payment p join seat s on p.seat_no = s.seat_no join movie_hall m on s.hall_name = m.hall_name join price_info pi on m.price_no = pi.price_no where p.cus_id = 'seongmin';";
+	String sql = "SELECT m.m_name, p.cus_id, m.hall_name, s.seat_no, m.m_start_time, m.m_end_time, (pi.adult_price*p.adult_member_count) + (pi.child_price*p.child_member_count), p.card_name, sysdate FROM payment p join seat s on p.seat_no = s.seat_no join movie_hall m on s.hall_name = m.hall_name join price_info pi on m.price_no = pi.price_no where p.cus_id = 'seongmin'";
 	
 	public void select() throws Exception{
 		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(null);
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+		
+		if(rs.next()) {
+			rs.getString("m_name");
+			
+			
+			
+			System.out.println(rs.getString("m_name"));
+		}
 	}
 	
 }
