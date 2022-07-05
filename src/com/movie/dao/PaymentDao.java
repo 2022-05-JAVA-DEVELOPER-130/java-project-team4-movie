bc484d9297ebc175180451da81c8ac40ea3b29c5
Juniverse <ggolre90@gmail.com>
2022-07-05 20:08:36 +0900

    PaymentDao - insert, create, delete 추가
    PaymentSQL - 수정

--- src/com/movie/dao/PaymentDao.java
+++ src/com/movie/dao/PaymentDao.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.movie.common.DataSource;
 import com.movie.dto.Payment;
+import com.movie.dto.Seat;
 
 
 public class PaymentDao {
@@ -20,17 +22,83 @@ public class PaymentDao {
 	/*
 	 * 주문전체삭제
 	 */
-	
+	public int deleteAllReservation(String cus_id) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+		
+		try {
+			con = dataSource.getConnection();
+			con.setAutoCommit(false);
+			pstmt = con.prepareStatement(PaymentSQL.PAYMENT_DELETE_BY_CUSID);
+			pstmt.setString(1, cus_id);
+			rowCount = pstmt.executeUpdate();
+			con.commit();
+		} catch (Exception e1) {
+			con.rollback();
+			e1.printStackTrace();
+		} finally {
+			if(con!=null)con.close();
+		}
+		return rowCount;
+	}
 	
 	/*
 	 * 주문 1건삭제
 	 */
-	
+	public int deleteByPaymentNo(int payment_no) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+		try {
+			con = dataSource.getConnection();
+			con.setAutoCommit(false);
+			pstmt = con.prepareStatement(PaymentSQL.PAYMENT_DELETE_BY_PAYMENTNO);
+			pstmt.setInt(1, payment_no);
+			rowCount = pstmt.executeUpdate();
+			con.commit();
+		} catch (Exception e1) {
+			con.rollback();
+			e1.printStackTrace();
+		} finally {
+			if(con!=null)con.close();
+		}
+		return rowCount;
+	}
 	
 	/*
 	 * 주문생성
 	 */
-	
+	public int createReservation(Payment payment) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt1 = null;
+		PreparedStatement pstmt2 = null;
+		try {
+			con = dataSource.getConnection();
+			con.setAutoCommit(false);
+			pstmt1 = con.prepareStatement(PaymentSQL.PAYMENT_INSERT);
+			pstmt1.setString(1, payment.getCard_name());
+			pstmt1.setInt(2, payment.getAdult_member_count());
+			pstmt1.setInt(3, payment.getChild_member_count());
+			pstmt1.setString(4, payment.getCus_id());
+			
+			pstmt2 = con.prepareStatement(PaymentSQL.PAYMENT_INSERT_BY_SEATNO);
+			for (Seat seat : payment.getSeatList()) {
+				pstmt2.setInt(1, seat.getSeat_arrange());
+				pstmt2.setInt(2, seat.getSeat_valid());
+				pstmt2.setString(3, seat.getHall_name());
+				pstmt2.executeQuery();
+			}
+		} catch (Exception e1) {
+			con.rollback();
+			e1.printStackTrace();
+		} finally {
+			if(con!=null)
+				con.close();
+		}
+		
+		return 0;
+	}
 	
 	/*
 	 * 주문전체(cus_id로 검색)
--- src/com/movie/dao/PaymentSQL.java
+++ src/com/movie/dao/PaymentSQL.java
@@ -15,14 +15,14 @@ public class PaymentSQL {
 	public final static String PAYMENT_DELETE_BY_CUSID = 
 		"delete FROM payment WHERE cus_id=?";
 
-	public final static String PAYMENT_DELETE_BY_SEATNO =
-	 "DELETE FROM payment WHERE seat_no=?";
+	public final static String PAYMENT_DELETE_BY_PAYMENTNO =
+	 "DELETE FROM payment WHERE payment_no=?";
 
 	public final static String PAYMENT_INSERT =
-	"INSERT INTO payment(pament_no, payment_date, card_name, adult_member_count, child_member_count, cus_id, seat_no) VALUES (payment_payment_no_SEQ.next, sysdate, ?, ?, ?, ?, ?)";
+	"INSERT INTO payment(pament_no, payment_date, card_name, adult_member_count, child_member_count, cus_id) VALUES (payment_payment_no_SEQ.next, sysdate, ?, ?, ?, ?)";
 
 	public final static String PAYMENT_INSERT_BY_SEATNO =
-	"INSERT INTO seat(seat_no, seat_arrange, seat_valid, hall_name, cus_id) VALUES (SEAT_SEAT_NO_SEQ.nextval, ?, ?, ?)";
+	"INSERT INTO seat(seat_no, seat_arrange, seat_valid, hall_name) VALUES (SEAT_SEAT_NO_SEQ.nextval, ?, ?, ?)";
 
 	public final static String SELECT_BY_CUSID =
 	"SELECT * FROM payment WHERE cus_id=?";
