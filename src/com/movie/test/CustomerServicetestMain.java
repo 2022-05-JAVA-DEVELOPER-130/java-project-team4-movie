8a410ee4fc3077d8562212957ea5c4ca529fbf24
hyeonjeongL <hunnnn94@gmail.com>
2022-07-05 20:47:10 +0900

    ..

--- src/com/movie/test/CustomerServicetestMain.java
+++ src/com/movie/test/CustomerServicetestMain.java
@@ -21,12 +21,6 @@ public class CustomerServicetestMain {
 		System.out.println("로그인>>" + cusService.login("hejin", "6789"));
 
 		System.out.println("회원탈퇴>>" + cusService.deleteInfo("hunjung"));
-
-//			Customer updateCustomer = 
-//					new Customer("updateid22", 0, "업데등장", "신수정", "1234", aaa, "F", "010-7895-4574",
-//				"updat22@naver.com", 3000, 1);
-//		System.out.println(">> 정보수정"
-//				+ cusService.updateInfo("업데등장", "1234", "1991/01/01", "F", "010-7895-4574", "updat22@naver.com"));
 		
 		Customer updateCustomer = new Customer ("newid22", 0, "점점점", "신수정", "1234", aaa, "M", "010-7895-4574",
 				"new22@naver.com", 3000, 1);
