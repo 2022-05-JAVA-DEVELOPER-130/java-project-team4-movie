6d584b99e9c54534db06f0315c20f9d60e85313b
seongmin <posteto@naver.com>
2022-07-06 13:36:26 +0900

    ..

--- src/com/movie/test/PriceTestMain.java
+++ src/com/movie/test/PriceTestMain.java
@@ -5,12 +5,6 @@ import com.movie.dto.PriceInfo;
 public class PriceTestMain {
 
 	public static void main(String[] args) throws Exception{
-		PriceInfo Price_no=new PriceInfo();
-		PriceInfo Adult_price=new PriceInfo();
-		PriceInfo Child_price=new PriceInfo();
-		System.out.println(Price_no);
-		System.out.println(Adult_price);
-		System.out.println(Child_price);
 		
 		}
 }
